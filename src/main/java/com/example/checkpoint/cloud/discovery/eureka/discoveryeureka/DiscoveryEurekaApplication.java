4b06a725f2593268d7500acd93f5c606fb042b69
cp <k.anees93@gmail.com>
2018-09-17 13:38:59 +0300

    configuration and working pom discovery service

--- src/main/java/com/example/checkpoint/cloud/discovery/eureka/discoveryeureka/DiscoveryEurekaApplication.java
+++ src/main/java/com/example/checkpoint/cloud/discovery/eureka/discoveryeureka/DiscoveryEurekaApplication.java
@@ -2,8 +2,11 @@ package com.example.checkpoint.cloud.discovery.eureka.discoveryeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryEurekaApplication {
 
 	public static void main(String[] args) {
