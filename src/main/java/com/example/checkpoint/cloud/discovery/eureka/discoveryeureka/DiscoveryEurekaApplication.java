0b92e2d2736d9ea1e73a9c221772495534de0057
cp <k.anees93@gmail.com>
2018-09-17 11:14:44 +0300

    first commit

---
+++ src/main/java/com/example/checkpoint/cloud/discovery/eureka/discoveryeureka/DiscoveryEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.example.checkpoint.cloud.discovery.eureka.discoveryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryEurekaApplication.class, args);
+	}
+}
